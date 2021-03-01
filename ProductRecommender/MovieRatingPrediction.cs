799b0f7e8cd3e189ee63031d0174f8ea2f4a6e42
MODERN122 <45830356+MODERN122@users.noreply.github.com>
2021-03-02 00:07:38 +0300

    added project for recommendation system

---
+++ ProductRecommender/MovieRatingPrediction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductRecommender
+{
+    public class MovieRatingPrediction
+    {
+        public float Label;
+        public float Score;
+    }
+}
---
+++ ProductRecommender/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Trainers;
+
+namespace ProductRecommender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+            (IDataView trainingDataView, IDataView testDataView) = LoadData(mlContext);
+
+        }
+        public static (IDataView training, IDataView test) LoadData (MLContext mlContext)
+        {
+            var trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-raiting-train.csv");
+            var testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-raitings-test.csv");
+
+            IDataView trainDataView = mlContext.Data.LoadFromTextFile<MovieRating>(trainDataPath, hasHeader: true, separatorChar: ',');
+            IDataView testDataView = mlContext.Data.LoadFromTextFile<MovieRating>(testDataPath, hasHeader: true, separatorChar: ',');
+            return (trainDataView, testDataView);
+        }
+    }
+}
