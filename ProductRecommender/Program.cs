52cb52a0528fbd37d4e16df7e8643a3610588aad
MODERN122 <45830356+MODERN122@users.noreply.github.com>
2021-03-02 14:24:20 +0300

    first step make recommendation system

--- ProductRecommender/Program.cs
+++ ProductRecommender/Program.cs
@@ -10,17 +10,57 @@ namespace ProductRecommender
         static void Main(string[] args)
         {
             MLContext mlContext = new MLContext();
-            (IDataView trainingDataView, IDataView testDataView) = LoadData(mlContext);
+            (IDataView trainDataView, IDataView testDataView) = LoadData(mlContext);
+            ITransformer model = BuildAndTrainModel(mlContext, trainDataView: trainDataView); 
+            EvaluateModel(mlContext, testDataView, model);
 
         }
         public static (IDataView training, IDataView test) LoadData (MLContext mlContext)
         {
-            var trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-raiting-train.csv");
-            var testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-raitings-test.csv");
+            var trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-train.csv");
+            var testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "recommendation-ratings-test.csv");
 
             IDataView trainDataView = mlContext.Data.LoadFromTextFile<MovieRating>(trainDataPath, hasHeader: true, separatorChar: ',');
             IDataView testDataView = mlContext.Data.LoadFromTextFile<MovieRating>(testDataPath, hasHeader: true, separatorChar: ',');
             return (trainDataView, testDataView);
+        }
+        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView trainDataView)
+        {
+            IEstimator<ITransformer> estimator = mlContext
+                .Transforms
+                .Conversion
+                .MapValueToKey(outputColumnName: "userIdEncoded", inputColumnName: "userId")
+                .Append(mlContext
+                    .Transforms
+                    .Conversion
+                    .MapValueToKey(outputColumnName: "movieIdEncoded", inputColumnName: "movieId"));
+            var options = new MatrixFactorizationTrainer.Options
+            {
+                MatrixColumnIndexColumnName = "userIdEncoded",
+                MatrixRowIndexColumnName = "movieIdEncoded",
+                LabelColumnName = "Label",
+                NumberOfIterations = 20,
+                ApproximationRank = 100
+            };
+            var trainerEstimator = estimator
+                .Append(mlContext
+                    .Recommendation()
+                    .Trainers
+                    .MatrixFactorization(options));
+            Console.WriteLine("========================== Training the model =============================");
+            ITransformer model = trainerEstimator.Fit(trainDataView);
+            return model;
+
+        }
+        public static void EvaluateModel(MLContext mlContext, IDataView testDataView, ITransformer model)
+        {
+            Console.WriteLine("========================== Evaluating the model =============================");
+            var prediction = model.Transform(testDataView);
+            var metrics = mlContext
+                .Regression
+                .Evaluate(prediction, labelColumnName: "Label", scoreColumnName: "Score");
+            Console.WriteLine("Root Mean Squared Error : " + metrics.RootMeanSquaredError.ToString());
+            Console.WriteLine("RSquared: " + metrics.RSquared.ToString());
         }
     }
 }
