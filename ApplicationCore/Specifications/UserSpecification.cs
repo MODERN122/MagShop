1a84f3fd0f483d1f87058dcc844f2e8a55f161d2
MODERN122 <whahnk@gmail.com>
2022-03-18 10:00:39 +0300

    Update UserSpecification.cs

--- ApplicationCore/Specifications/UserSpecification.cs
+++ ApplicationCore/Specifications/UserSpecification.cs
@@ -14,11 +14,31 @@ namespace ApplicationCore.Specifications
         /// <param name="userId"></param>
         public UserSpecification(string userId)
         {
-            Query.Where(x => x.Id == userId)
-                .Include(x=>x.Basket)
+            Query.Where(x => x.Id == userId);
+            Query.Include(x => x.Basket)
                 .ThenInclude(x => x.Items)
                 .ThenInclude(x => x.Product)
-                .ThenInclude(x=>x.Category);
+                .ThenInclude(x => x.ProductProperties)
+                .ThenInclude(x => x.ProductPropertyItems);
+
+            //Query.Include(x => x.Basket)
+            //    .ThenInclude(x => x.Items)
+            //    .ThenInclude(x => x.Product)
+            //    .ThenInclude(x => x.Category);
+            //Query.Include(x => x.Basket)
+            //    .ThenInclude(x => x.Items)
+            //    .ThenInclude(x => x.Product)
+            //    .ThenInclude(x => x.Images);
+
+            Query.Include(x => x.Basket)
+                .ThenInclude(x => x.Items)
+                .ThenInclude(x => x.Product)
+                .ThenInclude(p => p.Properties);
+
+            //Query.Include(x => x.Basket)
+            //    .ThenInclude(x => x.Items)
+            //    .ThenInclude(x => x.Product)
+            //    .ThenInclude(p => p.Store);
         }
     }
 }
